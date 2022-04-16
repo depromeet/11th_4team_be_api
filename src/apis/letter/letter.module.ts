9630cfbcb21271efb746caba8d4b2e306e002074
이찬진 <water0641@naver.com>
2022-04-16 12:12:21 +0900

    :rocket: fix(rooms) : finish room module

--- src/apis/letter/letter.module.ts
+++ src/apis/letter/letter.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
 import { LetterService } from './letter.service';
 import { LetterController } from './letter.controller';
+import { forwardRef, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Room, RoomSchema } from 'src/models/room.model';
 
+import { User, UserSchema } from 'src/models/user.model';
+import { UserModule } from '../users/user.module';
+import { LetterRepository } from 'src/repositories/letter.repository';
+import { Letter, LetterSchema } from 'src/models/letter.model';
+import { LetterRoom, LetterRoomSchema } from 'src/models/letterRoom.model';
 @Module({
-  providers: [LetterService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Room.name, schema: RoomSchema },
+      { name: User.name, schema: UserSchema },
+      { name: Letter.name, schema: LetterSchema },
+      { name: LetterRoom.name, schema: LetterRoomSchema },
+    ]),
+    forwardRef(() => UserModule),
+  ],
+  providers: [LetterService, LetterRepository],
   controllers: [LetterController],
+  exports: [LetterService, LetterRepository],
 })
 export class LetterModule {}
