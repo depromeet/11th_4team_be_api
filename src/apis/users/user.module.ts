86d8820a73ee0b792fd6a7f0f20e3ba8a3635aef
이찬진 <water0641@naver.com>
2022-05-04 23:34:57 +0900

    fix 파일명 대소문자,,,

--- src/apis/users/user.module.ts
+++ src/apis/users/user.module.ts
@@ -1,7 +1,7 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from 'src/auth/auth.module';
-import { Report, ReportSchema } from 'src/models/Report.model';
+import { Report, ReportSchema } from 'src/models/report.model';
 import { User, UserSchema } from 'src/models/user.model';
 import { ReportRepository } from 'src/repositories/report.repository';
 import { UserRepository } from 'src/repositories/user.repository';
