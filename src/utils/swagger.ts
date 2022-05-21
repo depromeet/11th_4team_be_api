eb87b372f34bcc0ee8903ceb9dfdce0300676298
이찬진 <water0641@naver.com>
2022-05-21 21:00:42 +0900

    :hammer: fix(joi) : add env validation

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -19,7 +19,7 @@ export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
     .setTitle('티키타카 api')
     .setDescription(swaggerInfo)
-    .setVersion('1.0.0')
+    .setVersion('0.3.0')
     // .addServer('api/v1')
     .addBearerAuth(
       {
