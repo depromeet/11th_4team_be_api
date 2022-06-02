5a945de4c49f142fd0824f4815d1c69cbdb687d0
이찬진 <water0641@naver.com>
2022-06-02 23:51:41 +0900

    :hammer: fix(room) : 위경도 지도상 minmax 값 지도 전체로 품

--- src/apis/rooms/dto/coordinates.dto.ts
+++ src/apis/rooms/dto/coordinates.dto.ts
@@ -4,13 +4,13 @@ import { IsNumber, Max, Min } from 'class-validator';
 export class CoordinatesDto {
   @ApiProperty({ example: '127', description: '경도입니다 세로선!' })
   @IsNumber()
-  @Min(120)
-  @Max(135)
+  @Min(-180)
+  @Max(180)
   lng: number;
 
   @ApiProperty({ example: '37', description: '위도입니다 가로선' })
   @IsNumber()
-  @Min(30)
-  @Max(45)
+  @Min(-90)
+  @Max(90)
   lat: number;
 }
